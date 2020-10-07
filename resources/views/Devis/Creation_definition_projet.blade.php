1e1c88f6c3103f89d0e959f2fb21d4f4b50ba830
Arthur CHERAMY <arthur.cheramy@gmail.com>
2020-10-07 09:20:53 +0200

    Update label

--- resources/views/Devis/Creation_definition_projet.blade.php
+++ resources/views/Devis/Creation_definition_projet.blade.php
@@ -26,14 +26,12 @@
         <div class="p-3">
             <form>
                 <div class="form-group">
-                    <h3 for="choixClient">Référence client : </h3>
+                    <label for="choixClient">Référence client : </label>
                     <input id="refClient" name="refClient" type="hidden" value="valeur de la ref client">
                     <div id="choixClient">
-                        <a href="#" class="btn btn-outline-primary btn-lg" role="button"
-                            style="background-color: white;">Client
+                        <a href="#" class="btn btn-outline-primary btn-lg btn-block" role="button">Client
                             existant</a>
-                        <a href="#" class="btn btn-outline-primary btn-lg" role="button"
-                            style="background-color: white;">Nouveau
+                        <a href="#" class="btn btn-outline-secondary btn-lg btn-block" role="button">Nouveau
                             client</a>
                     </div>
                 </div>
@@ -53,8 +51,8 @@
                     <input type="date" class="form-control" id="dateProjet">
                 </div>
 
-                <button type="submit" class="btn btn-primary">Suivant</button>
-                <button class="btn btn-outline-danger">Annuler</button>
+                <a role="button" class="btn btn-outline-danger" href="/home">Annuler</a>
+                <button type="submit" class="btn btn-primary float-right">Suivant</button>
             </form>
         </div>
     </div>
