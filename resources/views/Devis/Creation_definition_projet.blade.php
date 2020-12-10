41563f06ac119d5698c2248031c51578384a70cf
Arthur CHERAMY <arthur.cheramy@gmail.com>
2020-12-10 19:10:43 +0100

    Add modals for "définition projet"

--- resources/views/Devis/Creation_definition_projet.blade.php
+++ resources/views/Devis/Creation_definition_projet.blade.php
@@ -6,7 +6,8 @@
 @section('breadcrumb')
     <ol class="breadcrumb" style="border : 2px solid black; background-color : rgb(50, 85, 115); color:rgb(181, 215, 244)">
         <li class="breadcrumb-item"><a href="/home">Accueil</a></li>
-        <li class="breadcrumb-item" style="color:rgb(181, 215, 244)">Création d'un nouveau devis (Définition d'un nouveau projet)</li>
+        <li class="breadcrumb-item" style="color:rgb(181, 215, 244)">Création d'un nouveau devis (Définition d'un nouveau
+            projet)</li>
     </ol>
 @endsection
 
@@ -28,11 +29,14 @@
                 <div class="form-group">
                     <label for="choixClient">Référence client : </label>
                     <input id="refClient" name="refClient" type="hidden" value="valeur de la ref client">
-                    <div id="choixClient" style="display : flex; justify-content : center; align-items : center; flex-direction : column;">
-                        <a href="#" class="btn btn-outline-primary btn-lg btn-block" style="width : 70%; border: solid 2px" role="button">Client
-                            existant</a>
-                        <a href="#" class="btn btn-outline-secondary btn-lg btn-block" style="width : 70%; border : solid 2px"   role="button">Nouveau
-                            client</a>
+                    <div id="choixClient"
+                        style="display : flex; justify-content : center; align-items : center; flex-direction : column;">
+
+                        <button type="button" class="btn btn-outline-primary btn-lg btn-block" data-toggle="modal"
+                            data-target="#modalClientExistant">Client existant</button>
+                            
+                            <button type="button" class="btn btn-outline-secondary btn-lg btn-block" data-toggle="modal"
+                            data-target="#modalNouveauClient">Nouveau client</button>
                     </div>
                 </div>
 
@@ -56,5 +60,69 @@
             </form>
         </div>
     </div>
+
+    <!-- ModalClientExistant -->
+    <div class="modal fade" id="modalClientExistant" tabindex="-1" aria-labelledby="modalClientExistantTitre" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="modalClientExistantTitre">Sélection du client</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+                <ul class="list-group list-group">
+                    <li class="list-group-item active">Antoine CORGNIARD</li>
+                    <li class="list-group-item">Alexis Poupelin</li>
+                    <li class="list-group-item">Theo DAVID</li>
+                    <li class="list-group-item">Arthur CHERAMY</li>
+                  </ul>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+              <button type="button" class="btn btn-primary">Selectionner</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      
+      <!-- ModalClientExistant -->
+      <div class="modal fade" id="modalNouveauClient" tabindex="-1" aria-labelledby="modalNouveauClientTitre" aria-hidden="true">
+          <div class="modal-dialog">
+            <div class="modal-content">
+              <div class="modal-header">
+                <h5 class="modal-title" id="modalNouveauClientTitre">Création d'un nouveau client</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                  <span aria-hidden="true">&times;</span>
+                </button>
+              </div>
+              <div class="modal-body">
+                <form>
+                    <div class="form-group">
+                        <label for="nomPrenomInput">NOM et prenom du client</label>
+                        <input type="text" class="form-control" id="nomPrenomInput" placeholder="NOM Prenom">
+                      </div>
+                      <div class="form-group">
+                        <label for="mailInput">Adresse mail du client</label>
+                        <input type="email" class="form-control" id="mailInput" placeholder="nom.prenom@gmail.com">
+                      </div>
+                      <div class="form-group">
+                        <label for="adresseInput">Adresse du client</label>
+                        <input type="text" class="form-control" id="adresseInput" placeholder="78 rue du bois jolie, Le Mans, France...">
+                      </div>
+                      <div class="form-group">
+                        <label for="telephoneInput">Téléphone du client</label>
+                        <input type="text" class="form-control" id="telephoneInput" placeholder="06 11 22 33 44">
+                      </div>
+                  </form>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-dismiss="modal">Fermer</button>
+                <button type="button" class="btn btn-primary">Enregistrer</button>
+              </div>
+            </div>
+          </div>
+        </div>
 
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,14 @@
     <title>Madera</title>
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+        integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+    </script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous">
+    </script>
     @yield('css')
 
     <header>
@@ -32,10 +39,12 @@
 
     <div class="container">
         <nav aria-label="breadcrumb">
-                @yield('breadcrumb')
-            </nav>
-            @yield('body')
-        </div>
-    </body>
+            @yield('breadcrumb')
+        </nav>
+        @yield('body')
+    </div>
+</body>
 
-    </html>
+@yield("script")
+
+</html>
